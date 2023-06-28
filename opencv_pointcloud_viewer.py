be78e5a2bed629c3f13e0488ed1e8343f7f44662
Peter,Wilmot <paw114@scarletmail.rutgers.edu>
2023-06-28 15:01:38 +0000

    Update opencv_pointcloud_viewer.py

--- opencv_pointcloud_viewer.py
+++ opencv_pointcloud_viewer.py
@@ -22,10 +22,12 @@ Keyboard:
     [c]     Toggle color source
     [s]     Save PNG (./out.png)
     [e]     Export points to ply (./out.ply)
+    [f]     Export pointcloud to pickle files
     [q\ESC] Quit
 """
 
 import math
+import pickle
 import time
 import cv2
 import numpy as np
@@ -112,12 +114,6 @@ def mouse_cb(event, x, y, flags, param):
     if event == cv2.EVENT_RBUTTONUP:
         state.mouse_btns[1] = False
 
-    if event == cv2.EVENT_MBUTTONDOWN:
-        state.mouse_btns[2] = True
-
-    if event == cv2.EVENT_MBUTTONUP:
-        state.mouse_btns[2] = False
-
     if event == cv2.EVENT_MOUSEMOVE:
 
         h, w = out.shape[:2]
@@ -169,66 +165,6 @@ def view(v):
     """apply view transformation on vector array"""
     return np.dot(v - state.pivot, state.rotation) + state.pivot - state.translation
 
-
-def line3d(out, pt1, pt2, color=(0x80, 0x80, 0x80), thickness=1):
-    """draw a 3d line from pt1 to pt2"""
-    p0 = project(pt1.reshape(-1, 3))[0]
-    p1 = project(pt2.reshape(-1, 3))[0]
-    if np.isnan(p0).any() or np.isnan(p1).any():
-        return
-    p0 = tuple(p0.astype(int))
-    p1 = tuple(p1.astype(int))
-    rect = (0, 0, out.shape[1], out.shape[0])
-    inside, p0, p1 = cv2.clipLine(rect, p0, p1)
-    if inside:
-        cv2.line(out, p0, p1, color, thickness, cv2.LINE_AA)
-
-
-def grid(out, pos, rotation=np.eye(3), size=1, n=10, color=(0x80, 0x80, 0x80)):
-    """draw a grid on xz plane"""
-    pos = np.array(pos)
-    s = size / float(n)
-    s2 = 0.5 * size
-    for i in range(0, n+1):
-        x = -s2 + i*s
-        line3d(out, view(pos + np.dot((x, 0, -s2), rotation)),
-               view(pos + np.dot((x, 0, s2), rotation)), color)
-    for i in range(0, n+1):
-        z = -s2 + i*s
-        line3d(out, view(pos + np.dot((-s2, 0, z), rotation)),
-               view(pos + np.dot((s2, 0, z), rotation)), color)
-
-
-def axes(out, pos, rotation=np.eye(3), size=0.075, thickness=2):
-    """draw 3d axes"""
-    line3d(out, pos, pos +
-           np.dot((0, 0, size), rotation), (0xff, 0, 0), thickness)
-    line3d(out, pos, pos +
-           np.dot((0, size, 0), rotation), (0, 0xff, 0), thickness)
-    line3d(out, pos, pos +
-           np.dot((size, 0, 0), rotation), (0, 0, 0xff), thickness)
-
-
-def frustum(out, intrinsics, color=(0x40, 0x40, 0x40)):
-    """draw camera's frustum"""
-    orig = view([0, 0, 0])
-    w, h = intrinsics.width, intrinsics.height
-
-    for d in range(1, 6, 2):
-        def get_point(x, y):
-            p = rs.rs2_deproject_pixel_to_point(intrinsics, [x, y], d)
-            line3d(out, orig, view(p), color)
-            return p
-
-        top_left = get_point(0, 0)
-        top_right = get_point(w, 0)
-        bottom_right = get_point(w, h)
-        bottom_left = get_point(0, h)
-
-        line3d(out, view(top_left), view(top_right), color)
-        line3d(out, view(top_right), view(bottom_right), color)
-        line3d(out, view(bottom_right), view(bottom_left), color)
-        line3d(out, view(bottom_left), view(top_left), color)
 
 
 def pointcloud(out, verts, texcoords, color, painter=True):
@@ -315,9 +251,7 @@ while True:
 
     out.fill(0)
 
-    grid(out, (0, 0.5, 1), size=1, n=10)
-    frustum(out, depth_intrinsics)
-    axes(out, view([0, 0, 0]), state.rotation, size=0.1, thickness=1)
+    
 
     if not state.scale or out.shape[:2] == (h, w):
         pointcloud(out, verts, texcoords, color_source)
@@ -328,9 +262,7 @@ while True:
             tmp, out.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
         np.putmask(out, tmp > 0, tmp)
 
-    if any(state.mouse_btns):
-        axes(out, view(state.pivot), state.rotation, thickness=4)
-
+    
     dt = time.time() - now
 
     cv2.setWindowTitle(
@@ -355,6 +287,13 @@ while True:
 
     if key == ord("c"):
         state.color ^= True
+
+    if key == ord("f"):
+        Node = input("Type current node number.\n")
+        #out, verts, texcoords, color_source
+        pickle.dump(verts, open('verts'+Node+'.pkl','wb'))
+        pickle.dump(texcoords, open('texcoords'+Node+'.pkl','wb'))
+        pickle.dump(color_source, open('color_source'+Node+'.pkl','wb'))
 
     if key == ord("s"):
         cv2.imwrite('./out.png', out)
