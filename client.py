5f13c571c024bd7bca4b07a35f3d9ca8777ab029
Peter,Wilmot <paw114@scarletmail.rutgers.edu>
2023-07-19 16:27:44 +0000

    Update client.py

--- client.py
+++ client.py
@@ -15,16 +15,13 @@ def sendFile(filename):
     s.send(f"{filename}{SEPARATOR}{filesize}".encode())
     progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
     with open(filename, "rb") as f:
+        totalbytes = 0
         while True:
             # read the bytes from the file
             bytes_read = f.read(BUFFER_SIZE)
             if not bytes_read:
-                # file transmitting is done
                 break
-            # we use sendall to assure transimission in busy networks
             s.sendall(bytes_read)
-            # update the progress bar
-            progress.update(len(bytes_read))
     progress.close()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
