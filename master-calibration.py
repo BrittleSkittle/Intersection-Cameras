feb0a891eebc8ae55d4b6af11f57665fb6af171e
Peter,Wilmot <paw114@scarletmail.rutgers.edu>
2023-07-20 15:29:17 +0000

    Update master-calibration.py

--- master-calibration.py
+++ master-calibration.py
@@ -1,4 +1,12 @@
+import platform
+Node = platform.node().split('-')[0][4]
+print("Currently on Node "+Node+".\n")
+
 import corners
 import TwoDto3D
 import opencv_pointcloud_viewer
+import server
 import client
+import combine
+import static_pointcloud_viewer
+
